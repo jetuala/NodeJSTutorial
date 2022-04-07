4cfd75eca5f66bf1c8a3da48b55809137cb341c3
Jonathan Tuala <jetuala@gmail.com>
2022-04-06 23:05:30 -0700

    Picking up again after a break

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
-const _ = require('lodash');
+// const _ = require('lodash');
 
-const items = [1, [2, [3, [4]]]]
-const newItems = _.flattenDeep(items);
-console.log(newItems)
-console.log('hello everybody')
+// const items = [1, [2, [3, [4]]]]
+// const newItems = _.flattenDeep(items);
+// console.log(newItems)
+// console.log('hello everybody')
 
-// NEED TO INSTALL NODEMON GLOBALLY BEFORE MOVING ON
+const {readFile} = require('fs');
+
+const getText = (path) => {
+    return new Promise((resolve, reject) => {
+        readFile(path, 'utf8', (err, data) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve(data);
+            }
+        })
+    })
+}
+
+// getText('./content/first.txt')
+//     .then(result => console.log(result))
+//     .catch((err) => console.log(err))
+
+const start = async() => {
+    try {
+        const first = await getText('./content/first.txt')
+        console.log(first);
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start();
